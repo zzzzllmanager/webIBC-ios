716a0c94d81ec0232d1a321c45177cbe5ad22472
zzzlll <1728304585@qq.com>
2017-04-24 10:40:56 +0800

    项目创建

---
+++ WebIBC/WebIBC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebIBC
+//
+//  Created by zhaole on 2017/4/24.
+//  Copyright © 2017年 WEB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WebIBC/WebIBCTests/WebIBCTests.m
@@ -0,0 +1,39 @@
+//
+//  WebIBCTests.m
+//  WebIBCTests
+//
+//  Created by zhaole on 2017/4/24.
+//  Copyright © 2017年 WEB. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WebIBCTests : XCTestCase
+
+@end
+
+@implementation WebIBCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
