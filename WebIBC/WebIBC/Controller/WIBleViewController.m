73ca51564a388d3e1c038635973cef9193c22a05
zzzlll <1728304585@qq.com>
2017-04-26 08:59:40 +0800

    提交代码

--- WebIBC/WebIBC/Controller/WIBleViewController.m
+++ WebIBC/WebIBC/Controller/WIBleViewController.m
@@ -14,7 +14,7 @@
 
 #import "BabyBluetooth.h"
 
-#define BEACONUUID [[[UIDevice currentDevice] identifierForVendor] UUIDString]//iBeacon自己设备的uuid
+#define BEACONUUID @"12334566-7173-4889-9579-954995439125"//iBeacon自己设备的uuid
 
 @interface WIBleViewController () <CLLocationManagerDelegate,UITableViewDelegate,UITableViewDataSource>{
     BabyBluetooth *baby;
@@ -43,7 +43,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self setUpNaVc];
+    
     [self setUpTableview];
+}
+
+- (void)setUpNaVc
+{
+
+    UIBarButtonItem * rightItem = [[UIBarButtonItem alloc]initWithTitle:@"开始搜索" style:UIBarButtonItemStylePlain target:self action:@selector(rightItemClick)];
+    
+    self.navigationItem.rightBarButtonItem = rightItem;
+    
+    self.locationmanager = [[CLLocationManager alloc] init];//初始化
+    
+    self.locationmanager.delegate = self;
+    
+    [self.locationmanager requestAlwaysAuthorization];//设置location是一直允许
 }
 
 #pragma mark 初始化蓝牙设备
@@ -55,11 +71,13 @@
     tableview.dataSource = self;
     self.tableview = tableview;
     [self.view addSubview:tableview];
+    
+    
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 2;
+    return self.dataArray.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -68,94 +86,67 @@
     if(cell == nil){
         cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
     }
+    CLBeacon* beacon = self.dataArray[indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld",beacon.proximity];
     
-    if(indexPath.row == 0){
-    
-        cell.textLabel.text = @"点击搜索设备";
-    }else{
-        cell.textLabel.text = @"点击获取设备需要信息";
-    }
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    if(indexPath.row == 0){
-        //初始化BabyBluetooth 蓝牙库
-        baby = [BabyBluetooth shareBabyBluetooth];
-        
-        [baby cancelAllPeripheralsConnection];
     
-        //设置蓝牙委托
-        [baby setBlockOnCentralManagerDidUpdateState:^(CBCentralManager *central) {
-            if (central.state == CBCentralManagerStatePoweredOn) {
-               //设备打开成功
-            }
-        }];
-        //设置扫描到设备的委托
-        __weak typeof(self) weakSelf = self;
-        
-        [baby setBlockOnDiscoverToPeripherals:^(CBCentralManager *central, CBPeripheral *peripheral, NSDictionary *advertisementData, NSNumber *RSSI) {
-            NSLog(@"搜索到了设备:%@",peripheral.name);
-            
-            BOOL inArray = NO;
-            for (CBPeripheral * per in weakSelf.dataArray) {
-                if(per.identifier == peripheral.identifier){
-                    inArray = YES;
-                    break;
-                }
-            }
-            if(!inArray){
-                [weakSelf.dataArray addObject:peripheral];
-            }
-        }];
-        
-        //设置委托后直接可以使用，无需等待CBCentralManagerStatePoweredOn状态。
-        baby.scanForPeripherals().begin();
-        
-    }else{
-        
-        self.locationmanager = [[CLLocationManager alloc] init];//初始化
-        
-        self.locationmanager.delegate = self;
-    
-        [self.locationmanager requestAlwaysAuthorization];//设置location是一直允许
-    }
 }
 
-- (void)startMonitoringForRegionWithPer:(CBPeripheral*)per
+- (void) locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region
 {
-    CLBeaconRegion * beacon = [[CLBeaconRegion alloc] initWithProximityUUID:[[NSUUID alloc] initWithUUIDString:[per.identifier UUIDString]] identifier:@"media"];//初始化监测的iBeacon信息
-    [self.locationmanager startMonitoringForRegion:beacon];
+    [self.locationmanager requestStateForRegion:self.beacon];
 }
 
-- (void)startRangingBeaconsInRegionWithPer:(CBPeripheral*)per
+-(void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region
 {
-    CLBeaconRegion * beacon = [[CLBeaconRegion alloc] initWithProximityUUID:[[NSUUID alloc] initWithUUIDString:[per.identifier UUIDString]] identifier:@"media"];//初始化监测的iBeacon信息
-    [self.locationmanager startRangingBeaconsInRegion:beacon];
-}
-
-- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status{
     
-    if (status == kCLAuthorizationStatusAuthorizedAlways) {
-        //开始
-        for (CBPeripheral * per in self.dataArray) {
-            [self startMonitoringForRegionWithPer:per];
-        }
+    switch (state) {
+        case CLRegionStateInside:
+
+            [self.locationmanager startRangingBeaconsInRegion:(CLBeaconRegion *)region];
+            NSLog(@"INSIDE the Region");//not logging
+            break;
+        case CLRegionStateOutside:
+            NSLog(@"OUTSIDE the Region");
+            break;
+        case CLRegionStateUnknown:
+        default:
+            NSLog(@"Region state UNKNOWN!!!"); //Logging on console
+            [self.locationmanager startRangingBeaconsInRegion:(CLBeaconRegion *)region];
+            break;
+    }
+    if (state == CLRegionStateInside)
+    {
+        //Start Ranging
+        [self.locationmanager startRangingBeaconsInRegion:self.beacon];
+    }
+    else
+    {
+        //Stop Ranging here
+        [self.locationmanager startRangingBeaconsInRegion:self.beacon];
     }
 }
 
-//发现有iBeacon进入监测范围
--(void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region{
-    for (CBPeripheral * per in self.dataArray) {
-        [self startRangingBeaconsInRegionWithPer:per];
-    }
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region
+{
+    
 }
 
-//找的iBeacon后扫描它的信息
-- (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region{
-//    打印信息
+-(void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region
+{
+    
+}
+
+-(void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region
+{
+    if(beacons.count>0)
+    //    打印信息
     for (CLBeacon* beacon in beacons) {
         
         NSLog(@"rssi is :%ld",beacon.rssi);
@@ -165,7 +156,57 @@
         NSLog(@"beacon.major %@",beacon.major);
         
         NSLog(@"beacon.minor %@",beacon.minor);
+        
+        self.dataArray = [NSMutableArray arrayWithArray:beacons];
+        
+        [self.tableview reloadData];
     }
+}
+
+- (void)rightItemClick
+{
+    
+    self.beacon = [[CLBeaconRegion alloc]initWithProximityUUID:[[NSUUID alloc] initWithUUIDString:BEACONUUID] identifier:@"id"];
+    self.beacon.notifyOnEntry=YES;
+    self.beacon.notifyOnExit=YES;
+    self.beacon.notifyEntryStateOnDisplay=YES;
+    [self.locationmanager startMonitoringForRegion:self.beacon];
+    
+//    //初始化BabyBluetooth 蓝牙库
+//    baby = [BabyBluetooth shareBabyBluetooth];
+//    
+//    [baby cancelAllPeripheralsConnection];
+//    
+//    //设置蓝牙委托
+//    [baby setBlockOnCentralManagerDidUpdateState:^(CBCentralManager *central) {
+//        if (central.state == CBCentralManagerStatePoweredOn) {
+//            //设备打开成功
+//        }
+//    }];
+//    //设置扫描到设备的委托
+//    __weak typeof(self) weakSelf = self;
+//    
+//    [baby setBlockOnDiscoverToPeripherals:^(CBCentralManager *central, CBPeripheral *peripheral, NSDictionary *advertisementData, NSNumber *RSSI) {
+//        
+//        if(peripheral.name == nil||[peripheral.name isEqualToString:@""]){
+//            return;
+//        }
+//        NSLog(@"搜索到了设备:%@",peripheral.name);
+//        BOOL inArray = NO;
+//        for (CBPeripheral * per in weakSelf.dataArray) {
+//            if(per.identifier == peripheral.identifier){
+//                inArray = YES;
+//                break;
+//            }
+//        }
+//        if(!inArray){
+//            [weakSelf.dataArray addObject:peripheral];
+//            [weakSelf.tableview reloadData];
+//        }
+//    }];
+//
+//    //设置委托后直接可以使用，无需等待CBCentralManagerStatePoweredOn状态。
+//    baby.scanForPeripherals().begin();
 }
 
 @end
